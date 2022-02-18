937eab5c3d2a670f58a6f209f5094c4664848706
bigbl <adamkostandy@gmail.com>
2022-02-18 10:32:04 -0500

    Changed the overall look and fixed functionality for reading files from object and displaying onto screen. Working on coloring, might need to intergrate sass files with bootstrap.

--- routes/index.js
+++ routes/index.js
@@ -1,25 +1,29 @@
 let express = require('express');
 let router = express.Router();
 
+/* For now we will insert Book Objects to add new book descriptions. */
 
 const tempBookObjects = [
     {   name: "In The Miso Soup",
         date: "february",
         genre: "Horror",
         author: "Ryū Murakami",
-        people: 5
+        description: "In the Miso Soup tells of Frank, an overweight American tourist who has hired Kenji to take him on a guided tour of Tokyo’s sleazy nightlife",
+        image: "https://upload.wikimedia.org/wikipedia/en/a/a7/In_the_Miso_Soup_Cover.jpg"
     }, {
-        name: "In The Miso Soup2222",
-        date: "february",
+        name: "Im thinking of ending things",
+        date: "December/January 2021/2022",
         genre: "Horror",
-        author: "Ryū Murakami",
-        people: 5
+        author: "Ian Reid",
+        description: "Full of misgivings, a young woman travels with her new boyfriend to his parents' secluded farm. Upon arriving, she comes to question everything she thought she knew about him, and herself.",
+        image: "https://images-na.ssl-images-amazon.com/images/I/71UZvIul-+L.jpg"
     }, {
-        name: "In The Miso Soup111",
-        date: "february",
-        genre: "Horror",
-        author: "Ryū Murakami",
-        people: 5
+        name: "Perfume: The Story of a Murderer",
+        date: "June",
+        genre: "Thriller",
+        author: "Patrick Süskind",
+        description: "Perfume: The Story of a Murderer (German: Das Parfum: Die Geschichte eines Mörders [das paʁˈfœ̃ː diː ɡəˈʃɪçtə ˈʔaɪnəs ˈmœʁdɐs] (audio speaker iconlisten)) is a 1985 literary historical fantasy novel by German writer Patrick Süskind. The novel explores the sense of smell and its relationship with the emotional meanings that scents may have.",
+        image: "https://upload.wikimedia.org/wikipedia/en/f/f5/PerfumeSuskind.jpg"
     }
 ]
 
