2af887999e016277e781aca90d926217d1f3e8dd
bigbl <adamkostandy@gmail.com>
2022-03-31 13:49:31 -0400

    Push

--- public/scripts/aboutus.js
+++ public/scripts/aboutus.js
@@ -1,7 +1,6 @@
 const col = document.querySelector(".col-2")
 const h1 = document.createElement("h1");
-h1.innerText = "Adam"
-
+h1.innerText = "Aladdin"
 
 col.addEventListener("click", () => {
     col.appendChild(h1);
